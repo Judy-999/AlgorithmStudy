8de5d5b32f7a6bdc0aeedb06fab69787a735f3fe
Judy <joo00ee@naver.com>
2023-08-28 23:43:17 +0900

    [CHAPTER 04 구현] 실전 03 왕실의 나이트

---
+++ Judy/AlogithmStudy/AlogithmStudy/Study/Implement/Practice/night.swift
@@ -0,0 +1,26 @@
+//
+//  night.swift
+//  AlogithmStudy
+//
+//  Created by 김주영 on 2023/08/28.
+//
+
+import Foundation
+
+func night() {
+    let point = readLine()!.map { String($0) }
+    let alphavet = ["a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8]
+    let routes = [(2, 1), (-2, 1), (2, -1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+    let current = (alphavet[point[0]]!, Int(point[1])!)
+    var count = 0
+    
+    for route in routes {
+        let step = (route.0 + current.0, route.1 + current.1)
+        if step.0 < 1 || step.0 > 8 || step.1 < 1 || step.1 > 8 {
+            continue
+        }
+        count += 1
+    }
+    
+    print(count)
+}
