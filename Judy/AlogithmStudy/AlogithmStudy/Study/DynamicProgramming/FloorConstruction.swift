f1c5a0cc996026d8cde8c2964b927f8f8306af91
Judy <joo00ee@naver.com>
2023-11-08 23:45:49 +0900

    [CHAPTER 8 다이나믹 프로그래밍] 실전 4 바닥 공사

---
+++ Judy/AlogithmStudy/AlogithmStudy/Study/DynamicProgramming/FloorConstruction.swift
@@ -0,0 +1,42 @@
+//
+//  FloorConstruction.swift
+//  AlogithmStudy
+//
+//  Created by 김주영 on 2023/10/27.
+//
+
+import Foundation
+
+// 1 X 2, 2 X 1, 2 X 2
+
+func floorConstruction() {
+    let width = Int(readLine()!)!
+    var dic = [Int: Int]()
+    
+    dic[1] = 1
+    dic[2] = 3
+    
+    for num in 3..<(width + 1) {
+        if let n1 = dic[num - 1], let n2 = dic[num - 2] {
+            dic[num] = n1 + 2 * n2
+        }
+    }
+    
+    print(dic[width]! % 796796)
+}
+
+
+//for num in 3..<(width + 1) {
+//    var totalCount = 0
+//    for i in 1..<((num + 1) / 2) {
+//        if let count1 = dic[i], let count2 = dic[num - i] {
+//            totalCount += (count1 * count2)
+//        }
+//    }
+//
+//    totalCount *= 2
+//    if num % 2 == 0 {
+//        totalCount += 2 * (dic[num / 2] ?? 0)
+//    }
+//    dic[num] = totalCount
+//}
